4198d3a403e3c94b1f68a8bf477d32a124f4f258
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2023-07-11 15:15:40 +0530

    updating svelte config

--- src/components/OpenAI.svelte
+++ src/components/OpenAI.svelte
@@ -2,6 +2,7 @@
 	import { Configuration, OpenAIApi } from 'openai';
 
 	export function configureOpenAI() {
+		console.log('chorle api key: ', process.env.API_KEY);
 		const configuration = new Configuration({
 			apiKey: process.env.API_KEY
 		});
