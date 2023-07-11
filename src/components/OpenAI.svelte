7fad2a83ac6d23620594f39f071d290a27556738
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2023-07-11 15:36:56 +0530

    updating env var use

--- src/components/OpenAI.svelte
+++ src/components/OpenAI.svelte
@@ -2,7 +2,6 @@
 	import { Configuration, OpenAIApi } from 'openai';
 
 	export function configureOpenAI() {
-		console.log('chorle api key: ', process.env.API_KEY);
 		const configuration = new Configuration({
 			apiKey: process.env.API_KEY
 		});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv'
+dotenv.config();
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		port: 5100,
 		strictPort: false
-	}
+	},
+	define: {
+		"process.env.API_KEY": `"${process.env.API_KEY}"`,
+	  },
 });
